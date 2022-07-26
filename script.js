e0d77bcfe17c6771570a4132a85c0babf05c8f74
EdvinasStankunas <98836185+EdvinasStankunas@users.noreply.github.com>
2022-07-26 12:20:04 +0300

    Add resize button

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ let gridCount = 16;
 let grid = document.querySelector("#grid-container");
 const controls = document.querySelector("#controls");
 
+const gridCountBtn = document.querySelector("#resize-btn");
+gridCountBtn.textContent = "Enter size";
+gridCountBtn.addEventListener("click", () => {
+  gridCount = +prompt("Enter size value");
+  //console.log(gridCountPrompt);
+  let child = grid.lastElementChild;
+  while (child) {
+    grid.removeChild(child);
+    child = grid.lastElementChild;
+  }
+  genDivs();
+});
+
 /*
 Non functional reset button :)
 
